1f8c14646f540cd9ca3d1d7e266c05106c1623cd
Joakim Gyllenskepp <joakim.gyllenskepp@vegvesen.no>
2023-02-20 17:08:23 +0100

    Add an image on top of the puzzle

--- app/routes/puzzles/sliding_puzzle/puzzle.tsx
+++ app/routes/puzzles/sliding_puzzle/puzzle.tsx
@@ -1,6 +1,11 @@
 import { useSubmit } from "@remix-run/react";
 import type { Square } from "./utils";
 
+const widthHeight = 176;
+const imageUrl = `https://images.unsplash.com/photo-1598214886806-c87b84b7078b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=${
+  widthHeight * 3
+}&q=80&h=${widthHeight * 3}&q=80`;
+
 const SlidingPuzzle = ({ puzzle }: { puzzle: Square[] }) => {
   const submit = useSubmit();
 
@@ -10,12 +15,14 @@ const SlidingPuzzle = ({ puzzle }: { puzzle: Square[] }) => {
     submit(formData, { method: "post", action: "/puzzles/sliding_puzzle" });
   }
 
+  puzzle.forEach((sq) => console.log(sq.correctPosision, sq.empty));
+
   return (
     <div className="border-1 grid grid-cols-3 grid-rows-3 gap-4">
       {puzzle.map((square) => (
         <div className="h-44 w-44" key={`div-${square.id}`} />
       ))}
-      {puzzle.map((square) => (
+      {puzzle.map((square, index) => (
         <button
           className={`absolute transition-transform`}
           style={{ transform: getTranslation(square.position) }}
@@ -25,9 +32,19 @@ const SlidingPuzzle = ({ puzzle }: { puzzle: Square[] }) => {
           }}
         >
           {!square.empty && (
-            <p className={`h-44 w-44 ${square.color}`}>
-              {getLabel(square.correctPosision)}
-            </p>
+            <>
+              <div
+                className="absolute h-44 w-44"
+                style={{
+                  backgroundImage: `url(${imageUrl})`,
+                  backgroundPositionX: square.correctPosision.x * widthHeight,
+                  backgroundPositionY: square.correctPosision.y * widthHeight,
+                }}
+              />
+              <p className={`h-44 w-44 ${square.color}`}>
+                {getLabel(square.correctPosision)}
+              </p>
+            </>
           )}
         </button>
       ))}
